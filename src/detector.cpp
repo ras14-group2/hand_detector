b4a32ce5e6a1a8d6fa5e4f29136f60db3b41bb75
Marcel <geppert@ROSUbuntu.(none)>
2014-10-18 17:01:32 +0200

    refactored code to class

--- src/detector.cpp
+++ src/detector.cpp
@@ -10,152 +10,115 @@
 #include <cmath>
 
 
-//class Detector{
+class Detector{
 
-//private:
-//    ros::NodeHandle nh;
-//    ros::Publisher pub;
-//    ros::Subscriber sub;
+private:
+    ros::NodeHandle nh;
 
-//    pcl::PointCloud<pcl::PointXYZRGB> pointCloud;
-//    //pcl::MsgFieldMap fieldMap;
+    ros::Subscriber sub;
 
-//    //ros::Rate loop_rate(10);
+    ros::Publisher croppedCloudPub; //for debugging
+    ros::Publisher targetPub; //sends the detected target point
 
-//public:
+    pcl::PointCloud<pcl::PointXYZRGB> pointCloud; //the current pointcloud of the environment
+    pcl::CropBox<pcl::PointXYZRGB> cropBox; //filter to remove all parts outside a box
 
-//    Detector(int argc, char **argv){
-//        ros::init(argc, argv, "detector");
 
-//        //nh = ros::NodeHandle();
+    //extract a 3D box from the pointcloud
+    pcl::PointCloud<pcl::PointXYZRGB> maskPointCloud(){
 
-//        //sub = nh.subscribe("/camera/depth_registered/points", 1, pointcloudCallback);
-//        //pub = nh.advertise<geometry_msgs::Point>("hand-detector/target", 1);
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr tmp_ptr = pointCloud.makeShared();
+        cropBox.setInputCloud(tmp_ptr);
 
-//        //ros::Rate test_rate(10);
+        pcl::PointCloud<pcl::PointXYZRGB> newPointCloud;
+        cropBox.filter(newPointCloud);
 
-//        //loop_rate(10);
+        return newPointCloud;
+    }
 
-//        return;
-//    }
+    //finds the closest point to the basis
+    pcl::PointXYZ findClosestPoint(pcl::PointCloud<pcl::PointXYZRGB> pc){
 
-//    void pointcloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg){
+        pcl::PointXYZ closestPoint;
+        double minDistance = 100;
 
-//        //pcl::PCLPointCloud2 pcl_pc;
-//        //pcl::fromROSMsg(msg, pointCloud, fieldMap);
-//        //pcl::fromROSMsg(msg, pointCloud);
+        for(size_t i = 0; i < pc.points.size(); i++){
+            pcl::PointXYZRGB currPoint = pc.points[i];
+            double dist = std::sqrt(std::pow(currPoint.x, 2) + std::pow(currPoint.y, 2) + std::pow(currPoint.z, 2));
 
-//        return;
-//    }
+            if(dist < minDistance){
+                minDistance = dist;
+                closestPoint.x = currPoint.x;
+                closestPoint.y = currPoint.y;
+                closestPoint.z = currPoint.z;
+                //ROS_INFO("new min dist: %f", minDistance);
+            }
+        }
+        return closestPoint;
+    }
 
-//    //pcl::PointCloud extractObservationArea(const ros::PointCloud& pc){
+public:
 
-//        //pcl::PointCloud observedPoints;
-//        //return pcl::PointCloudl();
+    Detector(){
 
-//    //}
+        //TODO: change namespace??
+        nh = ros::NodeHandle();
 
-//    void printMaxVals(){
+        sub = nh.subscribe("/camera/depth_registered/points", 1, &Detector::pointcloudCallback, this);
+        croppedCloudPub = nh.advertise<sensor_msgs::PointCloud2>("hand_detector/cropped", 1);
+        targetPub = nh.advertise<geometry_msgs::Point>("hand_detector/target", 1);
 
-//        double left = 0;
-//        double righ = 0;
-//        double top = 0;
-//        double bottom = 0;
+        cropBox = pcl::CropBox<pcl::PointXYZRGB>();
 
-//        //int width = pointCloud.width;
-//        //int height = pointCloud.height;
+        cropBox.setMin(Eigen::Vector4f(-0.5, -0.5, 0.0, 1.0));
+        cropBox.setMax(Eigen::Vector4f(0.5, 0.5, 10.0, 1.0));
 
-////        for(size_t i = 0; i < width; i++){
-////            //top = std::max(top, (double) pointCloud.points[i].data_c[1]);
-////        }
-////        //ROS_INFO("top: %d", top);
+        return;
+    }
 
-////        for(size_t i = 0; i < width; i++){
-////            bottom = std::min(bottom, (double) pointCloud.points[((height-1) * width) + i].data_c[1]);
-////        }
-////        //ROS_INFO("bottom: %d", bottom);
-
-////        for(size_t i = 0; i < height; i++){
-////            //left = std::min(left, (double) pointCloud.points[i * width].data_c[0]);
-////        }
-////        //ROS_INFO("left: %d", left);
-
-////        for(size_t i = 0; i < height; i++){
-////            //right = std::max(right, (double) pointCloud.points[(i * width) + width - 1].data_c[0]);
-////        }
-//        //ROS_INFO("right: %d", right);
-//    }
-
-//    void sleep(){
-
-//        //loop_rate.sleep();
-//        return;
-//    }
-
-//};
-
-    pcl::PointCloud<pcl::PointXYZRGB> pointCloud;
-
+    //callback function to receive messages
     void pointcloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg){
+
         pcl::fromROSMsg(*msg, pointCloud);
 
         return;
     }
 
-int main(int argc, char **argv){
 
-    //Detector detector(argc, argv);
+    //the main function that detects the hand (or object) in the pointcloud
+    void detectHand(){
+
+        pcl::PointCloud<pcl::PointXYZRGB> croppedPointCloud = maskPointCloud();
+
+        pcl::PointXYZ closestPoint = findClosestPoint(croppedPointCloud);
+
+        sensor_msgs::PointCloud2 croppedCloudMsg;
+        pcl::toROSMsg(croppedPointCloud, croppedCloudMsg);
+        croppedCloudPub.publish(croppedCloudMsg);
+
+        geometry_msgs::Point targetPoint;
+        targetPoint.x = closestPoint.x;
+        targetPoint.y = closestPoint.y;
+        targetPoint.z = closestPoint.z;
+        targetPub.publish(targetPoint);
+
+        return;
+    }
+
+};
+
+
+int main(int argc, char **argv){
 
     ros::init(argc, argv, "detector");
 
-    ros::NodeHandle nh;
-
-    ros::Subscriber sub = nh.subscribe("/camera/depth_registered/points", 1, pointcloudCallback);
-    ros::Publisher croppedpub = nh.advertise<sensor_msgs::PointCloud2>("hand_detector/cropped", 1);
-    ros::Publisher targetpub = nh.advertise<geometry_msgs::Point>("hand_detector/target", 1);
-
-    pcl::CropBox<pcl::PointXYZRGB> cropBox;
-
-    cropBox.setMin(Eigen::Vector4f(-0.5, -0.2, 0.5, 1.0));
-    cropBox.setMax(Eigen::Vector4f(0.5, 0.5, 1.5, 1.0));
+    Detector detector;
 
     ros::Rate loop_rate(5);
 
     while(ros::ok()){
 
-        pcl::PointCloud<pcl::PointXYZRGB>::Ptr tmp_ptr = pointCloud.makeShared();
-
-        cropBox.setInputCloud(tmp_ptr);
-
-        pcl::PointCloud<pcl::PointXYZRGB> newPointCloud;
-
-        cropBox.filter(newPointCloud);
-
-        pcl::PointXYZ nearestPoint;
-        double minDistance = 10;
-
-        for(size_t i = 0; i < newPointCloud.points.size(); i++){
-            pcl::PointXYZRGB currPoint = newPointCloud.points[i];
-            double dist = std::sqrt(std::pow(currPoint.x, 2) + std::pow(currPoint.y, 2) + std::pow(currPoint.z, 2));
-
-            if(dist < minDistance){
-                minDistance = dist;
-                nearestPoint.x = currPoint.x;
-                nearestPoint.y = currPoint.y;
-                nearestPoint.z = currPoint.z;
-                ROS_INFO("new min dist: %f", minDistance);
-            }
-        }
-
-        sensor_msgs::PointCloud2 croppedCloudMsg;
-        pcl::toROSMsg(newPointCloud, croppedCloudMsg);
-        croppedpub.publish(croppedCloudMsg);
-
-        geometry_msgs::Point targetPoint;
-        targetPoint.x = nearestPoint.x;
-        targetPoint.y = nearestPoint.y;
-        targetPoint.z = nearestPoint.z;
-        targetpub.publish(targetPoint);
+        detector.detectHand();
 
         ros::spinOnce();
 
