1f03ee012ee26a429b3df2ab0987e5f0efce6566
mgeppert <geppert@kth.se>
2014-10-21 00:29:36 +0200

    added additional filters

--- src/detector.cpp
+++ src/detector.cpp
@@ -1,11 +1,13 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Point.h>
-
 #include <sensor_msgs/PointCloud2.h>
+
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/crop_box.h>
 #include <pcl/filters/approximate_voxel_grid.h>
+#include <pcl/filters/median_filter.h>
+
 #include <pcl_conversions/pcl_conversions.h>
 
 #include <cmath>
@@ -23,7 +25,9 @@ private:
 
     pcl::PointCloud<pcl::PointXYZ> pointCloud; //the current pointcloud of the environment
     pcl::CropBox<pcl::PointXYZ> cropBox; //filter to remove all parts outside a box
-    pcl::ApproximateVoxelGrid<pcl::PointXYZ> voxelGrid; //downsample data
+    pcl::ApproximateVoxelGrid<pcl::PointXYZ> fineGrid; //downsample data
+    pcl::ApproximateVoxelGrid<pcl::PointXYZ> coarseGrid; //downsample data
+    pcl::MedianFilter<pcl::PointXYZ> medianFilter; //median filter to remove outliners
 
 
     //extract a 3D box from the pointcloud
@@ -38,20 +42,44 @@ private:
         return newPointCloud;
     }
 
-    //downsample the given pointcloud
-    pcl::PointCloud<pcl::PointXYZ> downSample(const pcl::PointCloud<pcl::PointXYZ>& pc){
+    //downsample the given pointcloud with fine grid
+    pcl::PointCloud<pcl::PointXYZ> sampleFineGrid(const pcl::PointCloud<pcl::PointXYZ>& pc){
 
         pcl::PointCloud<pcl::PointXYZ>::Ptr tmp_ptr = pc.makeShared();
-        voxelGrid.setInputCloud(tmp_ptr);
+        fineGrid.setInputCloud(tmp_ptr);
 
         pcl::PointCloud<pcl::PointXYZ> newPointCloud;
-        voxelGrid.filter(newPointCloud);
+        fineGrid.filter(newPointCloud);
+
+        return newPointCloud;
+    }
+
+    //downsample the given pointcloud with coarse grid
+    pcl::PointCloud<pcl::PointXYZ> sampleCoarseGrid(const pcl::PointCloud<pcl::PointXYZ>& pc){
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr tmp_ptr = pc.makeShared();
+         coarseGrid.setInputCloud(tmp_ptr);
+
+        pcl::PointCloud<pcl::PointXYZ> newPointCloud;
+        coarseGrid.filter(newPointCloud);
+
+        return newPointCloud;
+    }
+
+    //apply median filter to remove outliners
+    pcl::PointCloud<pcl::PointXYZ> removeOutliners(const pcl::PointCloud<pcl::PointXYZ>& pc){
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr tmp_ptr = pc.makeShared();
+        medianFilter.setInputCloud(tmp_ptr);
+
+        pcl::PointCloud<pcl::PointXYZ> newPointCloud;
+        medianFilter.filter(newPointCloud);
 
         return newPointCloud;
     }
 
     //finds the closest point to the origin
-    pcl::PointXYZ findClosestPoint(pcl::PointCloud<pcl::PointXYZ> pc){
+    pcl::PointXYZ findClosestPoint(const pcl::PointCloud<pcl::PointXYZ>& pc){
 
         pcl::PointXYZ closestPoint;
         double minDistance = 100;
@@ -81,13 +109,18 @@ public:
         targetPub = nh.advertise<geometry_msgs::Point>("hand_detector/target", 1);
 
         cropBox = pcl::CropBox<pcl::PointXYZ>();
-
         cropBox.setMin(Eigen::Vector4f(-0.5, -0.5, 0.0, 1.0));
         cropBox.setMax(Eigen::Vector4f(0.5, 0.5, 10.0, 1.0));
 
-        voxelGrid = pcl::ApproximateVoxelGrid<pcl::PointXYZ>();
+        fineGrid = pcl::ApproximateVoxelGrid<pcl::PointXYZ>();
+        fineGrid.setLeafSize(0.05, 0.05, 0.02);
 
-        voxelGrid.setLeafSize(0.1, 0.1, 0.05);
+        coarseGrid = pcl::ApproximateVoxelGrid<pcl::PointXYZ>();
+        coarseGrid.setLeafSize(0.1, 0.1, 0.02);
+
+        medianFilter = pcl::MedianFilter<pcl::PointXYZ>();
+
+
 
         return;
     }
@@ -105,12 +138,15 @@ public:
     void detectHand(){
 
         pcl::PointCloud<pcl::PointXYZ> croppedPointCloud = maskPointCloud(pointCloud);
-        pcl::PointCloud<pcl::PointXYZ> downSampledPointCloud = downSample(croppedPointCloud);
+        pcl::PointCloud<pcl::PointXYZ> fineGridPointCloud = sampleFineGrid(croppedPointCloud);
+        pcl::PointCloud<pcl::PointXYZ> cleanedPointCloud = removeOutliners(fineGridPointCloud);
+        pcl::PointCloud<pcl::PointXYZ> coarseGridPointCloud = sampleCoarseGrid(cleanedPointCloud);
 
-        pcl::PointXYZ closestPoint = findClosestPoint(downSampledPointCloud);
+
+        pcl::PointXYZ closestPoint = findClosestPoint(coarseGridPointCloud);
 
         sensor_msgs::PointCloud2 dsCloudMsg;
-        pcl::toROSMsg(downSampledPointCloud, dsCloudMsg);
+        pcl::toROSMsg(coarseGridPointCloud, dsCloudMsg);
         croppedCloudPub.publish(dsCloudMsg);
 
         geometry_msgs::Point targetPoint;
